2100096dcfd74bdb4063116e104fea4f1800abf0
PeterKow <peter.kowalczyk@aurity.co>
2015-11-07 21:09:14 +0000

    session storagrage map

--- src/app/persistance/storage.js
+++ src/app/persistance/storage.js
@@ -3,19 +3,19 @@ let storage = {}
 init()
 
 export function put(key, value) {
-  window.localStorage.setItem(key, value)
+  storage.setItem(key, value)
 }
 
 export function get(key) {
-  return window.localStorage.getItem(key)
+  return storage.getItem(key)
 }
 
 export function remove(key) {
-  return window.localStorage.removeItem(key)
+  return storage.removeItem(key)
 }
 
 export function clear() {
-  window.localStorage.clear()
+  storage.clear()
 }
 
 
