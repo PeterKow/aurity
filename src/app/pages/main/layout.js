349b74f1533bb29037bd14ea8827bdd7bbeae93e
Paweł Walczewski <pawel.walczewski@unity.pl>
2015-11-12 20:51:23 +0100

    removing obsolete imports

--- src/app/pages/main/layout.js
+++ src/app/pages/main/layout.js
@@ -3,11 +3,6 @@ import React, { Component } from 'react';
 import Navbar from 'containers/navbar/navbar.js'
 
 
-const List = require('material-ui/lib/lists/list');
-const ListItem = require('material-ui/lib/lists/list-item');
-const ListDivider = require('material-ui/lib/lists/list-divider');
-const Avatar = require('material-ui/lib/avatar');
-
 //required by material-ui. Supposed to be removed, as soon as react v1.0 is published
 let injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
